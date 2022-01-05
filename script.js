ce806cd6b015f736a89daaee2dfd3f40bb9c4735
Alwin Nedrum <nedrum@gmail.com>
2022-01-05 11:43:47 +0100

    added AsciiFX class

--- script.js
+++ script.js
@@ -5,8 +5,28 @@ const image1 = new Image();
 image1.src = "../img/ALWN1426.JPG";
 image1.height = 500;
 image1.width = image1.height * 1.333;
+
+class AsciiFX {
+  #imageCellArr = [];
+  #symbols = [];
+  #pixels = [];
+  #ctx;
+  #width;
+  #height;
+  constructor(ctx, width, height) {
+    this.#ctx = ctx;
+    this.#width = width;
+    this.#height = height;
+
+    this.#ctx.drawImage(image1, 0, 0, this.#width, this.#height);
+  }
+}
+
+let effect;
+
 image1.onload = init = () => {
   canvas.width = image1.width;
   canvas.height = image1.height;
-  ctx.drawImage(image1, 0, 0, image1.width, image1.height);
+  //   ctx.drawImage(image1, 0, 0, image1.width, image1.height);
+  effect = new AsciiFX(ctx, image1.width, image1.height);
 };
